1f1d472d47647edada801b853742226091d9df16
celina <celinaoliveira98@gmail.com>
2023-09-14 22:04:02 -0300

    testes classe usuario

--- src/usuario.test.js
+++ src/usuario.test.js
@@ -1,14 +1,22 @@
-const Usuario = require('./usuario');
+const { Usuario } = require('./usuario');
 
 describe('Classe Usuario', () => {
   let usuario;
-
   beforeEach(() => {
     usuario = new Usuario('João', 'joao@email.com', 25, '12345678900');
   });
 
   it('deve retornar o nome do usuário', () => {
     expect(usuario.nome).toBe('João');
+  });
+
+  it('deve atualizar o nome do usuário', () => {
+    usuario.setNome('Maria');
+    expect(usuario.nome).toBe('Maria');
+  });
+
+  it('deve retornar o email do usuário', () => {
+    expect(usuario.email).toBe('joao@email.com');
   });
 
 });
